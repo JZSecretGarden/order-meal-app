9d309a16f54b88e87437c0d5bd353e2515ca668d
17341986338 <2779644405qq.com>
2023-01-13 10:52:29 +0800

    搭建图片目录

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,26 +1,18 @@
 <!-- 外卖页面 -->
 <template>
 	<view>
-		<button @click="testPort">测试接口</button>
 	</view>
 </template>
 
 <script>
-	import {$fetch_index_entry} from "../../apis/port.js"
 	export default {
 		data() {
 			return {
-				id:1
+				
 			}
 		},
 		methods: {
-			testPort:function(){
-				$fetch_index_entry({
-					geohash:"31.22967,121.4762"
-				}).then(res=>{
-					console.log(res)
-				})
-			}
+			
 		}
 	}
 </script>
