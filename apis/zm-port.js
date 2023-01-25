b1973f12324d9c26c23114aaec730ff02bc14d79
GRZM <1993065070@qq.com>
2023-01-26 00:02:31 +0800

    食品分类搜索 60%

--- apis/zm-port.js
+++ apis/zm-port.js
@@ -1,2 +1,11 @@
 //引入req
 import req from './request'
+
+// 食品分类列表 v2/index_entry
+export const $fetch_indexentry = ()=>req('v2/index_entry','Get')
+
+// 获取商铺列表 shopping/restaurants
+export const $fetch_restaurants = (query)=>req('shopping/restaurants','Get',query)
+
+// 搜索餐馆 v4/restaurants
+export const $fetch_searchrestaurants = (query)=>req('v4/restaurants','Get',query)
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -6,20 +6,55 @@
 			<view>搜索</view>
 		</view>
 		<view class="search-box-input">
-		   搜索地址
+		   <u-input v-model="keyword" type="text" style="width: 70%;background-color: #f2f2f2;" placeholder="请输入商家或美食名称"/>
+		   <u-button type="primary" style="width: 20%;margin-left: 10px;" @click="FuncKeyword">提交</u-button>
+		</view>
+		<view class="search-box-result">
+		   <h4>商家</h4>
+		   <ul>
+			   <li class="result-li">
+				   <image src="../../static/search/logo.png"></image>
+				   <view class="result-li-right">
+				   	  <text>fdgfg</text>
+					  <text>月售 841 单</text>
+					  <text>20 元起送 / 距离2200.5公里</text>
+				   </view>
+		       </li>
+			   <li class="result-li">
+			   				   <image src="../../static/search/logo.png"></image>
+			   				   <view class="result-li-right">
+			   				   	  <text>fdgfg</text>
+			   					  <text>月售 841 单</text>
+			   					  <text>20 元起送 / 距离2200.5公里</text>
+			   				   </view>
+			   </li>
+			  
+		   </ul>
 		</view>
 	</view>
 </template>
 
 <script>
+	import {$fetch_searchrestaurants} from '@/apis/zm-port.js'
 	export default {
 		data() {
 			return {
-				
+				keyword: '',
+								
+								
 			}
 		},
+		onLoad() {
+			 // 搜索餐馆
+			 // 参数为 geohash string经纬度 keyword string关键词
+			 // $fetch_searchrestaurants().then(res=>{
+				//  console.log(res)
+			 // })
+		},
 		methods: {
-			
+			FuncKeyword(){
+				console.log(this.keyword)
+			}
 		}
 	}
 </script>
@@ -47,8 +82,57 @@
 	text-align: center;
 }
 .search-box-input{
+	    width: 100%;
+	    padding: 20px 10px 20px 10px;
+	    background-color: white;
+	    display: flex;
+	    justify-content: space-between;
+	    box-sizing: border-box;
+}
+.search-box-result{
 	width: 100%;
-	padding: 45px 10px 5px 10px;
+	
+}
+.search-box-result>h4{
+	height: 50px;
+	    line-height: 50px;
+	    padding: 5px;
+	    box-sizing: border-box;
+	    font-size: 18px;
+	    color: #666;
+	    background-color: #f2f2f2;
+}
+.search-box-result>ul{
+	width: 100%;
+	padding: 5px;
+	box-sizing: border-box;
 	background-color: white;
+}
+.result-li{
+	width: 100%;
+	list-style: none;
+	padding: 10px 5px;
+	box-sizing: border-box;
+	border-bottom: 1px solid #f2f2f2;
+	display: flex;
+	justify-content: space-between;
+	
+}
+.result-li>image{
+	width: 100px;
+	height: 100px;
+	
+}
+.result-li-right{
+	width: 70%;
+	border-bottom: 1px solid #ede8e8f0;
+	display: flex;
+	flex-direction: column;
+	font-size: 18px;
+	font-weight: bold;
+}
+.result-li-right>text{
+	height: 45px;
+	line-height: 45px;
 }
 </style>
