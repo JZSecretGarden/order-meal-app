5c1bde304f0dd647a7a8ef42b3d89a052ba361f5
失乐少年 <2970267960@qq.com>
2023-01-26 21:54:03 +0800

    更改shop页面

--- apis/jz-port.js
+++ apis/jz-port.js
@@ -8,3 +8,5 @@ export const $comment_class =  (data)=>req('ugc/v2/restaurants/'+data+'/ratings/
 export const $comment_content = (id,data)=>req('ugc/v2/restaurants/'+id+'/ratings','Get',data)
 //获取食品分类列表
 export const $food_class = ()=>req('v2/index_entry','Get')
+//获取评价分数
+export const $comment_scores = (id)=>req('ugc/v2/restaurants/'+id+'/ratings/scores','Get')
