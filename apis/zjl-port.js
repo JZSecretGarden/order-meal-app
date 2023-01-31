1e811f3b107758d91737aba7b338191c20935821
“18071838336” <18071838336@163.com>
2023-01-31 11:38:05 +0800

    “1111”

--- apis/zjl-port.js
+++ apis/zjl-port.js
@@ -1,2 +1,8 @@
 //引入req
 import req from './request'
+// 获取城市列表
+export const $getCityList = (obj)=> req('/v1/cities','get',obj);
+// 搜索地址
+export const $searchAddress = (obj)=> req('/v1/pois','get',obj);
+//获取收货地址列表
+export const $addressGetGood = (id)=> req('/v1/users/'+id+'/addresses','get');
